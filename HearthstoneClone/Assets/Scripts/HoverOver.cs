1866fa222e24e9062f45dd17ac6bbf80039faabf
Patrick Schuur <patrickschuurdev@gmail.com>
2023-10-09 21:09:25 +0200

    Fixed hover detection

--- HearthstoneClone/Assets/Scripts/HoverOver.cs
+++ HearthstoneClone/Assets/Scripts/HoverOver.cs
@@ -1,60 +1,45 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using Cards.Interfaces.MouseOver;
 using UnityEngine;
 
-namespace DefaultNamespace
+namespace Engine
 {
 	public class HoverOver : MonoBehaviour
 	{
-		private List<IMouseOver> _mouseOvers = new List<IMouseOver>();
-		
+		private Camera mainCamera;
+
+		private Transform hoveringOver;
+		private List<IMouseOver> mouseOvers = new();
+
+		private void Awake()
+		{
+			mainCamera = Camera.main;
+		}
+
 		public void FixedUpdate()
 		{
-			if (Camera.main != null)
+			Ray ray = mainCamera.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out RaycastHit hit, 100))
 			{
-				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-				if (Physics.Raycast (ray, out RaycastHit hit, 100))
+				if (hit.transform == hoveringOver) return;
+
+				hoveringOver = hit.transform;
+				mouseOvers.ForEach(pOver =>
 				{
-
-					var components = hit.transform.GetComponents(typeof(Component));
-
-					var a = components.Select(pComponent =>
-					{
-						if (pComponent.GetType().GetInterface(typeof(IMouseOver).ToString()) == typeof(IMouseOver))
-							return pComponent.GetType();
-						return null;
-					});
-
-					foreach (var b in a)
-					{
-						if (b == null) continue;
-						
-						Component component = hit.transform.GetComponent(b);
-						IMouseOver mouseOver = (IMouseOver) component;
-						
-						if (_mouseOvers.Contains(mouseOver)) continue;
-						_mouseOvers.Add(mouseOver);
-						mouseOver.OnStartHover();
-						mouseOver.IsHovering = true;
-					}
-
-					List<IMouseOver> toBeRemoved = new List<IMouseOver>();
-					
-					foreach (var mouseOver in _mouseOvers)
-					{
-						if (a == null || !a.Contains(mouseOver.GetType()))
-						{
-							mouseOver.OnEndHover();
-							mouseOver.IsHovering = false;
-							
-							toBeRemoved.Add(mouseOver);
-						}
-					}
-					
-					toBeRemoved.ForEach(pMouseOver => _mouseOvers.Remove(pMouseOver));
-				}
+					pOver.OnEndHover();
+					pOver.IsHovering = false;
+				});
+				
+				mouseOvers = hit.transform.GetComponents<IMouseOver>().ToList();
+				
+				mouseOvers.ForEach(pOver =>
+				{
+					pOver.OnStartHover();
+					pOver.IsHovering = true;
+				});
 			}
 		}
 	}
